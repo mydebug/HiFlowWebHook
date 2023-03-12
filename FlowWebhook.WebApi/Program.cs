dde7fede6ca315ceded2d3a7c4794c27003cde40
chenc <94575594@qq.com>
2023-03-12 21:06:44 +0800

    增加缓存存/取

--- FlowWebhook.WebApi/Program.cs
+++ FlowWebhook.WebApi/Program.cs
@@ -8,17 +8,38 @@ namespace FlowWebhook.WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddMemoryCache();
             var app = builder.Build();
-            // 发送数据到hiflow webhook 
+            var cache = app.Services.GetService<IMemoryCache>();
+            // 发送数据到webhook 
             app.MapPost("/{id}", async ([FromRoute] string id, [FromBody] object o) =>
             {
-                var webHookUrl = "https://api.hiflow.tencent.com/engine/webhook/31/" + id;
-                HttpClient client = new HttpClient();
-                var result = await client.PostAsJsonAsync(webHookUrl, o);
+                await SendDataAsync(id, o);
                 return Results.Ok();
             });
-            app.MapGet("/test", () => { return "nihao"; });
+
+            // 添加缓存数据
+            app.MapPost("cache/{name}", ([FromRoute] string name, [FromBody] object o) =>
+            {
+                cache.Set(name, o);
+                return Results.Ok();
+            });
+
+            // 获取缓存数据
+            app.MapGet("cache/{name}", ([FromRoute] string name) =>
+            {
+                return cache.Get(name);
+            });
+
+            app.MapGet("/", () => { return "hello,chendaye"; });
             app.Run();
+
+        }
+        private static async Task SendDataAsync(string id, object o)
+        {
+            var webHookUrl = "https://api.hiflow.tencent.com/engine/webhook/31/" + id;
+            HttpClient client = new HttpClient();
+            var result = await client.PostAsJsonAsync(webHookUrl, o);
         }
     }
 }
